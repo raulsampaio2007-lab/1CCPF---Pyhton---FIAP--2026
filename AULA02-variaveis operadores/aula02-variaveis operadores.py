b014d9e7c9f5da60ed459e1a626b8c8ad25e56b0
raulsampaio2007-lab <raulsampaio2007@gmail.com>
2026-03-17 11:41:15 -0300

    aula 02

---
+++ AULA02-variaveis operadores/aula02-variaveis operadores.py
@@ -0,0 +1,28 @@
+from winerror import RPC_S_UNKNOWN_AUTHN_LEVEL
+
+print("Ola mundo")
+
+print(7 + 4)
+
+print("7 "+ "4")
+
+print("7+4") # concatenação de strings
+
+""""""
+#VARIÁVEIS
+nome = "Raul "
+idade = 18
+peso = 83
+print(nome, idade, peso)
+print(f"Oiii, {nome}!")
+# input -- simula formas no CMD
+nome= input("Digite seu nome: ")
+peso= input("Digite seu peso")
+idade= input("Digite sua idade")
+print(nome, idade, peso)
+print(f"Seja bem vindo {nome}!")
+nome= "João"
+print(nome)
+print(f"Seja bem vindo{nome}")
+
+
